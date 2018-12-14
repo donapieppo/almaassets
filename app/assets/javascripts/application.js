a0e55778a84c82958c87d206a7d0c41dbc5bef82
Donapieppo <donapieppo@yahoo.it>
2018-12-14 16:38:09 +0100

    typeahead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require dm_unibo_common
 //= require activestorage
 //= require turbolinks
+//= require typeahead.bundle
 //= require_tree .
