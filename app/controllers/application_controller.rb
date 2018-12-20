98b4fa75b6c57d9810fe1d15bf9e04cc46027355
Donapieppo <donapieppo@yahoo.it>
2018-12-20 10:41:34 +0100

    pundit policies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,17 @@ class ApplicationController < ActionController::Base
   impersonates :user
 
   before_action :log_current_user, :force_sso_user
+  after_action :verify_authorized, except: [:index, :who_impersonate, :impersonate]
 
   def current_organization
-    @current_organization ||= Organization.first
-    @current_organization
+    # tmp TODO FIXME
+    session[:oid] = 1 unless session[:oid]
+
+    if session[:oid]
+      @current_organization = Organization.find(session[:oid].to_i)
+    else
+      redirect_to choose_current_organization
+    end
   end
 
   def check_user_can_admin
