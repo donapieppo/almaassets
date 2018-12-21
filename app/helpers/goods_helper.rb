55acf7a899db99adf572bdbd05a3a47d2dc02e93
Donapieppo <donapieppo@yahoo.it>
2018-12-21 17:00:14 +0100

    old_inv_number helper

--- app/helpers/goods_helper.rb
+++ app/helpers/goods_helper.rb
@@ -13,4 +13,11 @@ module GoodsHelper
     icon = good.to_unload ? icon('times-circle') : icon('trash-alt', prefix: 'far')
     link_to icon, unload_good_path(good), remote: true
   end
+
+  def old_inv_number(good)
+    return "" unless good.old_inv_number 
+    content_tag :span, class: "badge badge-secondary p-1" do
+      good.old_inv_number.to_s + " - " + good.old_org.to_s
+    end
+  end
 end
