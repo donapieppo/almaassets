39fcd45bd1b227a2353bb756c535484a43728281
Donapieppo <donapieppo@yahoo.it>
2019-01-09 13:28:03 +0100

    :new_confirm, :confirm, :new_unconfirm, :unconfirm

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :goods do
     get 'find', on: :collection
     get 'unload', on: :member
+
+    get  'new_confirm',   on: :member
+    post 'confirm',       on: :member
+    get  'new_unconfirm', on: :member
+    post 'unconfirm',     on: :member
   end
 
   resources :categories do 
