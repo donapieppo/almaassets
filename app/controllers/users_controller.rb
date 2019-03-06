e929634f6e4d30a307d284b673382d685a4f1352
Donapieppo <donapieppo@yahoo.it>
2019-03-06 09:39:48 +0100

    syncronize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
     @users = User.order(:surname, :name)
     render layout: false
   end
+
+  def new
+  end
+
+  def create
+    if r = /\A(\w+\.\w+)(@unibo.it)?\z/.match(params[:upn])
+      upn = r[1] + '@unibo.it'
+      begin
+        User.syncronize(upn)
+      rescue DmUniboCommon::NoUser
+        flash[:alert] = "Non esiste l'utente selezionato nel database di Ateneo."
+      end
+    end
+    redirect_to new_user_path
+  end
 end
