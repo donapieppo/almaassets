3775383c644a5161e63c63b9a38d22ce7b4da585
Donapieppo <donapieppo@yahoo.it>
2019-03-28 16:06:17 +0100

    fund holder

--- app/models/good_request.rb
+++ app/models/good_request.rb
@@ -2,6 +2,7 @@ class GoodRequest < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :category, optional: true
   belongs_to :main_agreement, optional: true
+  belongs_to :holder, class_name: 'User', foreign_key: :holder_id, optional: true
 
   def to_s
     self.name
