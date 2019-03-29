63b3f4975a0e60f43346966f9d6de53783032c71
Donapieppo <donapieppo@yahoo.it>
2019-03-29 15:38:18 +0100

    new request

--- app/controllers/good_requests_controller.rb
+++ app/controllers/good_requests_controller.rb
@@ -2,31 +2,46 @@ class GoodRequestsController < ApplicationController
   before_action :set_good_request_and_check_permission, only: [:show, :edit, :update]
 
   def index
-    @good_requests = current_user.good_requests
+    @good_requests = GoodRequest.order('users.surname, users.name, category_id').includes(:category, :user)
+    @good_requests = @good_requests.where(user: current_user) unless current_user.is_admin?
+    authorize :good_request
   end
 
   def new 
-    @category = Category.find(params[:category_id])
-    @available_agreements = @category.main_agreements.all
+    @good_request = GoodRequest.new
 
-    if params[:main_agreement_id]
-      @main_agreement = MainAgreement.find(params[:main_agreement_id]) 
-    elsif params[:outside_agreements]
-      @outside_agreements = true
+    @good_request.category = Category.find(params[:category_id]) if params[:category_id]
+    @good_request.main_agreement = MainAgreement.find(params[:main_agreement_id]) if params[:main_agreement_id]
+    @outside_agreements = params[:outside_agreements]
+
+    if @good_request.category
+      @available_agreements = @good_request.category.main_agreements.all 
     end
-    @good_request = @category.good_requests.new
+
     authorize @good_request
   end
 
   def create
     @category = Category.find(params[:category_id])
     @good_request = @category.good_requests.new(good_request_params)
-    @good_request.user = current_user
+    @good_request.user = current_user unless user_admin?
     authorize @good_request
     if @good_request.save
-      redirect_to goods_path, notice: "La richiesta è stata creato correttamente."
+      redirect_to root_path, notice: "La richiesta è stata creato correttamente."
     else
       render action: :new
+    end
+  end
+
+  def edit
+    @main_agreement = @good_request.main_agreement
+  end
+
+  def update
+    if @good_request.update_attributes(good_request_params)
+      redirect_to edit_good_request(@good_request) and return
+    else
+      render :edit
     end
   end
 
@@ -38,6 +53,8 @@ class GoodRequestsController < ApplicationController
   end
 
   def good_request_params
-    params[:good_request].permit(:category_id, :main_agreement_id, :name, :description, :derogation)
+    p = [:category_id, :main_agreement_id, :holder_id, :name, :description, :derogation, :max_price]
+    (p << :user_id) if user_admin?
+    params[:good_request].permit(p)
   end
 end
