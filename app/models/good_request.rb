4f17f8bee4ea648419125e3deed3b921d02f7c9c
Donapieppo <donapieppo@yahoo.it>
2019-04-09 12:27:53 +0200

    attr_accessor :outside_agreements

--- app/models/good_request.rb
+++ app/models/good_request.rb
@@ -4,6 +4,8 @@ class GoodRequest < ApplicationRecord
   belongs_to :main_agreement, optional: true
   belongs_to :holder, class_name: 'User', foreign_key: :holder_id, optional: true
 
+  attr_accessor :outside_agreements
+  
   def to_s
     self.name
   end
