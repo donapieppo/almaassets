fecbdbd43bb667a6b3f1c3c24157d2e4cf807a0b
Donapieppo <donapieppo@yahoo.it>
2019-04-10 11:00:18 +0200

    default_scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord 
   include DmUniboCommon::User
 
+  default_scope { order(:surname, :name) }
+
   has_many :goods
   has_many :good_requests
 
   belongs_to :organization, optional: true
+
 end
 
 
