c266c537d0928134ce7234912c7ab74fb5496faa
Donapieppo <donapieppo@yahoo.it>
2019-06-28 13:57:02 +0200

    validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :server
   belongs_to :user
 
+  validates :start_at, uniqueness: { scope: :server_id, message: "L'intervallo è già prenotato." }
+
   scope :future, -> { where('start_at > NOW()') }
   scope :in_week, -> { where('start_at BETWEEN CURDATE() AND (CURDATE() + INTERVAL 7 DAY)') }
-
 end
 
