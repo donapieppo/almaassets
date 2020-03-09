dfa2ccd1c96aa1a22f5b0bce9fc3bf3398052121
Donapieppo <donapieppo@yahoo.it>
2020-03-09 17:42:38 +0100

    permissions

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,9 @@
 class Organization < ApplicationRecord
   include DmUniboCommon::Organization
 
+  has_many :permissions, class_name: "DmUniboCommon::Permission"
+
+  has_many :locations
   has_many :goods
   has_many :good_requests
 end
