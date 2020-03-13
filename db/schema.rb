2d5da0da4a2c7e69e377625e8e71a4059413d5fc
Donapieppo <donapieppo@yahoo.it>
2020-03-13 13:01:02 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_110147) do
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "bookings", id: :integer, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "organization_id", unsigned: true
@@ -153,6 +174,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_110147) do
     t.index ["upn"], name: "index_upn_on_users", length: 191
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "servers", name: "fk_bookings_servers", on_delete: :cascade
   add_foreign_key "bookings", "users", name: "fk_bookings_users", on_delete: :cascade
   add_foreign_key "documents", "main_agreements", name: "fk_document_main_agreement"
