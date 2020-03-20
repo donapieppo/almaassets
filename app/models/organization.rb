605d32646e619987b1153bf3caf8d964d8c3fc73
Donapieppo <donapieppo@yahoo.it>
2020-03-20 17:46:43 +0100

    moved in DmUniboCommon

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,6 @@
 class Organization < ApplicationRecord
   include DmUniboCommon::Organization
 
-  has_many :permissions, class_name: "DmUniboCommon::Permission"
-
   has_many :locations
   has_many :goods
   has_many :good_requests
