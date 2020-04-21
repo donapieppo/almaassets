f75f96e0aefa684ec28ee6a54202c74c6af6fc7c
Donapieppo <donapieppo@yahoo.it>
2020-04-21 14:21:29 +0200

    organization

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    if policy(current_organization).edit?  
+    if current_organization && policy(current_organization).manage?  
       redirect_to good_requests_path and return
-    else
+    elsif current_organization
       @good_requests = current_user.good_requests
+    else
+      redirect_to root_path(__org__: 'mat')
     end
   end
 
