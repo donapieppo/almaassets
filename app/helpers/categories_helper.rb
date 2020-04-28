e633d6e3cb2eb03c7f7b14fe42f57f9ce5dbf3dc
Donapieppo <donapieppo@yahoo.it>
2020-04-28 12:12:44 +0200

    audio

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -25,6 +25,8 @@ module CategoriesHelper
       "<i class='fas fa-code' style='font-size: #{size}px'></i>".html_safe
     when 'server'
       "<i class='fas fa-server' style='font-size: #{size}px'></i>".html_safe
+    when 'audio'
+      "<i class='fas fa-music' style='font-size: #{size}px'></i>".html_safe
     when 'network'
       "<i class='fas fa-network-wired' style='font-size: #{size}px'></i>".html_safe
     when 'other'
