d59fa6793cf547b870d100520d80b1f796d1e6f4
Donapieppo <donapieppo@yahoo.it>
2020-05-04 17:06:21 +0200

    good_requests_as_holder

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
     if current_organization && policy(current_organization).manage?  
       redirect_to good_requests_path and return
     elsif current_organization
-      @good_requests = current_user.good_requests
+      @good_requests = current_user.good_requests.all
+      @good_requests_as_holder = GoodRequest.where(holder_id: current_user.id).all
     else
       redirect_to root_path(__org__: 'mat')
     end
