f23f4f420bbbc366f5e71f7c66a09501245508b8
Donapieppo <donapieppo@yahoo.it>
2020-08-21 13:24:44 +0200

    moved in dm_unibo_common

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  include Pundit
-
-  include DmUniboCommon::Controllers::Helpers
-
-  impersonates :user
-
-  before_action :log_current_user, :force_sso_user, :set_organization, :update_current_user_authlevels
+class ApplicationController < DmUniboCommon::ApplicationController
+  before_action :force_sso_user
   after_action :verify_authorized, except: [:index, :who_impersonate, :impersonate, :shibboleth]
-
-  def default_url_options(_options={})
-    _options[:__org__] = current_organization ? current_organization.code : nil
-    _options
-  end
 end
