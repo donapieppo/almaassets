a45c7a16a2bea702d15983d8ea08c72d50211f1f
Donapieppo <donapieppo@yahoo.it>
2020-08-28 11:40:43 +0200

    choose_organization

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,14 @@ class HomeController < ApplicationController
       @good_requests_as_holder = GoodRequest.where(holder_id: current_user.id).all
       @good_requests_as_holder -= @good_requests 
     else
-      redirect_to root_path(__org__: 'mat')
+      redirect_to choose_organization_path and return
+    end
+  end
+
+  def choose_organization
+    authorize :home
+    if current_user_has_some_authorization?
+      @organizations = current_user.my_organizations
     end
   end
 
