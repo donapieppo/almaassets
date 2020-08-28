456841e91c299096496e222365a14556b63dd4cd
Donapieppo <donapieppo@yahoo.it>
2020-08-28 11:40:28 +0200

    before_action in new dm_unibo_common

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < DmUniboCommon::ApplicationController
-  before_action :force_sso_user
+  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user
   after_action :verify_authorized, except: [:index, :who_impersonate, :impersonate, :shibboleth]
 end
