bc3ae9fab869e7098c026b816b8fd0b48a07aee7
Pietro <donapieppo@yahoo.it>
2022-02-16 10:23:46 +0100

    rails7

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//import "controllers"
+import * as bootstrap from "bootstrap"
+import Awesomplete from "awesomplete"
+
+import Rails from "@rails/ujs"
+Rails.start()
