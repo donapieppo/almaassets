335a9571e16b3d44745ce29ce00b9cad0d5a578a
Pietro <donapieppo@yahoo.it>
2022-05-27 11:14:01 +0200

    clean

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Almaassets
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     config.hosts << "tester.dm.unibo.it"
@@ -19,13 +18,5 @@ module Almaassets
     config.action_mailer.default_url_options = {protocol: 'https'}
 
     config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
