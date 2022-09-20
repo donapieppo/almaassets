433d62233f967cceb8180cb233c250a368b5d264
Pietro <donapieppo@yahoo.it>
2022-09-20 11:29:01 +0200

    @current_user

--- app/components/good/good_component.rb
+++ app/components/good/good_component.rb
@@ -1,11 +1,12 @@
 class Good::GoodComponent < ViewComponent::Base
   def initialize(good, current_user, no_icon: false, search_string: nil)
     @good = good
+    @current_user = current_user
     @no_icon = no_icon
     @search_string = search_string
 
-    @good_policy = GoodPolicy.new(current_user, @good)
-    @manager  = OrganizationPolicy.new(current_user, current_user.current_organization).manage?
+    @good_policy = GoodPolicy.new(@current_user, @good)
+    @manager = OrganizationPolicy.new(@current_user, @current_user.current_organization).manage?
     @found = @search_string && @good.inv_number == @search_string.to_i
   end
 end
