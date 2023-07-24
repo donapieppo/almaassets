1d7ff77d25076c81c0f602ec7a304822113f6074
Pietro <donapieppo@yahoo.it>
2023-07-24 11:51:38 +0200

    after_current_user_and_organization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < DmUniboCommon::ApplicationController
-  before_action :set_current_user, :update_authorization, :set_current_organization, :log_current_user, :force_sso_user
+  before_action :set_current_user,
+    :update_authorization,
+    :set_current_organization,
+    :after_current_user_and_organization,
+    :log_current_user,
+    :force_sso_user
   after_action :verify_authorized, except: [:who_impersonate, :impersonate, :shibboleth]
+
+  def after_current_user_and_organization
+  end
 end
